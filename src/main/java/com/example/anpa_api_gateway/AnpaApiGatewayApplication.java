b78e59a42292f002736f8c7d83e9da986250e259
Spaciuk, Valentin EXT <valentin.spaciuk@comafi.com.ar>
2026-02-05 15:29:28 -0300

    first commit

---
+++ src/main/java/com/example/anpa_api_gateway/AnpaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.anpa_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnpaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnpaApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/anpa_api_gateway/AnpaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anpa_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnpaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
